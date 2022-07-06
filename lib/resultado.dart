b00b701509fa37939e68fb0450a9055b6d1f4b1d
Victor Antunes <bpxlvictor@gmail.com>
2022-07-06 12:59:59 -0300

    Adicionado pontuação e alinhamento da frase final

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,15 +1,44 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  const Resultado({Key? key}) : super(key: key);
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns!';
+    } else if (pontuacao < 12) {
+      return "Você é bom!";
+    } else if (pontuacao < 16) {
+      return "Impressionante!";
+    } else {
+      return "Nível Jedi!";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Parabéns!',
-        style: TextStyle(fontSize: 28),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+        FlatButton(
+          child: Text(
+            'Reiniciar?',
+            style: TextStyle(fontSize: 28),
+          ),
+          textColor: Colors.blue,
+          onPressed: quandoReiniciarQuestionario,
+        )
+      ],
     );
   }
 }
